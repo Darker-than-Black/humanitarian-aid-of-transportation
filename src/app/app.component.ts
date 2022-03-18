98a40a2af5af62315118a33523cf516a2a1e39b7
Dmytro Rybachek <Puss.in.Boots@i.ua>
2022-03-18 12:50:12 +0200

    add base template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ApiService } from './services/api.service';
+import { StoreService } from './services/store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'humanitarian-aid-of-transportation';
+export class AppComponent implements OnInit {
+  constructor(
+    public store: StoreService,
+    private apiService: ApiService,
+  ) {}
+
+  loading: boolean = false;
+
+  ngOnInit() {
+    // this.loading = true;
+
+    this.apiService.getList();
+    // .subscribe(() => {
+    //   this.loading = false;
+    // });
+  }
 }
